6debec2026f28a2b2b7ad8170a2a06d0bb03ebfe
Lưu Tuấn Anh <139638161+tuananh190@users.noreply.github.com>
2025-09-05 23:07:42 +0700

    Thêm tính năng JWT authentication

---
+++ src/main/java/com/mar/CRUD_SERVICE/service/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.mar.CRUD_SERVICE.service;
+
+import com.mar.CRUD_SERVICE.dto.request.AuthenticationRequest;
+import com.mar.CRUD_SERVICE.dto.request.RegisterRequest;
+import com.mar.CRUD_SERVICE.dto.request.AuthenticationResponse;
+import com.mar.CRUD_SERVICE.entity.User;
+import com.mar.CRUD_SERVICE.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+
+        var user = User.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .dob(request.getDob())
+                .role("USER")
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .authenticated(true)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .authenticated(true)
+                .build();
+    }
+}
